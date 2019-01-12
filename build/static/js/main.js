2eb1f5fd1c546668899406851fcc085cf16a393a
DanilVod <43678372+DanilVod@users.noreply.github.com>
2019-01-12 22:41:46 +0300

    Попытался сделать бургер
    Попытка номер 1

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
     $('.slider').slick()
 });
+
+$(document).ready(function ($) {
+    $(function() {
+        $('.menu-mobile').on('click', function() {
+            $(this).closet('.menu').toggleClass('menu-open');
+        });
+    });
+})(jQuery);
