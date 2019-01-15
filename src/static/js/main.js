2f7d6ae89d29e8decda89fb6f65ba07ca74ae800
DanilVod <43678372+DanilVod@users.noreply.github.com>
2019-01-15 18:26:11 +0300

    Начало табов

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -5,3 +5,31 @@ $(document).ready(function ($){
         });
     });
 });
+
+$(" .tabs__content__item").not(":first").hide();
+
+var tabIndex = 0, //индекс текущего таба
+    countTabs = $(".tabs__content__item").length - 1, //количество табов
+    time = 5000; // время перелистывания, установленно 1сек
+
+$(".tabs.portfolio__tabs .tabs__block .tab").click(function () {
+  tabIndex = $(this).index();
+  checkTab();
+});
+
+setInterval(function(){
+  tabIndex++;
+  if(tabIndex > countTabs){
+    tabIndex = 0;
+  }
+  checkTab();
+}, time);
+
+function checkTab(){
+  $(".tab").removeClass("isActive").eq(tabIndex).addClass("isActive");
+    $(".tabs.portfolio__tabs .tabs__content__item")
+    .hide()
+    .eq(tabIndex)
+    .fadeIn();
+}
+
