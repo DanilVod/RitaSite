54aace7b263be0d69ac9c05e50c223f3b68aeff1
DanilVod <43678372+DanilVod@users.noreply.github.com>
2019-01-30 20:38:02 +0300

    Сделал слайдер(v1)

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,29 +1,46 @@
-const ALL_TABS_ID = "all";
+const ALL_TABS_ID = 'all';
 
 function menuOnClick() {
   $(this)
-    .closest(".header__menu")
-    .toggleClass("menu-open");
-};
-
+    .closest('.header__menu')
+    .toggleClass('menu-open');
+}
 function tabsOnClick() {
-  const tabId = $(this).data("tab");
+  const tabId = $(this).data('tab');
   const content = $(`.portfolio-item[data-tab=${tabId}]`);
-
-  $(".tab").removeClass("isActive");
-  $(this).addClass("isActive");
-
+  $('.tab').removeClass('isActive');
+  $(this).addClass('isActive');
   if (tabId === ALL_TABS_ID) {
-    return $(".portfolio-item").removeClass("hidden");
+    return $('.portfolio-item').removeClass('hidden');
   }
-  $(".portfolio-item").addClass("hidden");
-  $(content).removeClass("hidden");
-};
-
-$(document).ready(function($) {
-  $(".menu__icon").on("click", menuOnClick);
-  $(".tab").on("click", tabsOnClick);
+  $('.portfolio-item').addClass('hidden');
+  $(content).removeClass('hidden');
+}
+$(document).ready(($) => {
+  $('.menu__icon').on('click', menuOnClick);
+  $('.tab').on('click', tabsOnClick);
+  $('.slider-block').slick({
+    centerMode: true,
+    centerPadding: '-10px',
+    slidesToShow: 1,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+      },
+    },
+    ],
+  });
 });
-
-
-
